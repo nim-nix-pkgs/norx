affd4cea413a01fb4aba6e4e522f2f9b425574c4
hugosenari <hugosenari@gmail.com>
2022-03-12 04:34:57 -0300

    chore: re index norx

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''A wrapper of the ORX 2.5D game engine'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."norx-master".url = "path:./master";
+  inputs."norx-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norx-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norx-0_4_2".url = "path:./0_4_2";
+  inputs."norx-0_4_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norx-0_4_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."norx-0_4_3".url = "path:./0_4_3";
+  inputs."norx-0_4_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."norx-0_4_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A wrapper of the ORX 2.5D game engine'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-norx-master.flake = false;
+  inputs.src-norx-master.owner = "gokr";
+  inputs.src-norx-master.ref   = "refs/heads/master";
+  inputs.src-norx-master.repo  = "norx";
+  inputs.src-norx-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-norx-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-norx-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
